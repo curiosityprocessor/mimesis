b77f12ec121f0a0bda1120b696c84dc9056f0d81
cyjoo <38976452+cyjoo@users.noreply.github.com>
2021-06-30 00:46:45 +0900

    create sample pages, update resource samples

--- flutter_instagram/lib/util/constant.dart
+++ flutter_instagram/lib/util/constant.dart
@@ -1,6 +1,6 @@
 
-const String profile = "https://instagram.fpnh11-1.fna.fbcdn.net/v/t51.2885-19/s320x320/68684319_401052720532854_5518657891571073024_n.jpg?_nc_ht=instagram.fpnh11-1.fna.fbcdn.net&_nc_ohc=6EW5c0cWvm0AX-vPL2h&tp=1&oh=9d1ffb6249f3bf18b5afbfedd9430308&oe=6019CF02";
-const String username = "sathyamony";
+const String profile = "https://picsum.photos/id/1079/200/300";
+const String username = "CuriosityProcessor";
 
-const String instagramName = "Sela Monysathya";
-const String instagramBio = "I ᗩᗰ ᑎOT ᑭEᖇᖴEᑕT ᗷᑌT \nI ᗩᗰ ᒪIᗰITEᗪ EᗪITIOᑎ. \nᗰOOᑎᑕᕼIᒪᗪ 🌙";
+const String instagramName = "Curiosity Processor";
+const String instagramBio = "processing...";
